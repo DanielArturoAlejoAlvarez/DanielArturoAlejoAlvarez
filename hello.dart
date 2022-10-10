a10a42d74b0c0d5a57dd9ca8bcfe3042c7aed6a1
Daniel Arturo Alejo Alvarez <18106359+DanielArturoAlejoAlvarez@users.noreply.github.com>
2022-10-10 10:45:13 -0500

    Update hello.dart
    Add more data to file hello.dart

--- hello.dart
+++ hello.dart
@@ -5,4 +5,6 @@ void main() {
   print("I’m currently learning Mobile Apps with Dart and Flutter.");
   print("I’m looking to collaborate on Software projects that help companies improve their efficiency and productivity.");
   print("I’m looking for help with You're open to new job opportunities.");
+  print("All of my projects are available at https://github.com/DanielArturoAlejoAlvarez.");
+  print("I regularly write articles on https://pe.linkedin.com/in/danielarturoalejoalvarez");
 }
