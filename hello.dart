49b77ac66d2a4a983aff184ba669ea6380980c45
Daniel Arturo Alejo Alvarez <18106359+DanielArturoAlejoAlvarez@users.noreply.github.com>
2022-10-10 10:40:20 -0500

    Update hello.dart
    Add more text plain data.

--- hello.dart
+++ hello.dart
@@ -1,4 +1,6 @@
 void main() {
   print("Hi wave, I'm Daniel Arturo Alejo Alvarez.");
   print("A passionate software developer from Perú.");
+  print("I’m currently working on MediasoftDev");
+  print("I’m currently learning Mobile Apps with Dart and Flutter.");
 }
