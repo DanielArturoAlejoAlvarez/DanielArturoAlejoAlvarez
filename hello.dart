73e253cfe0794e46c6e542c2c0a7d8d7854db3e3
Daniel Arturo Alejo Alvarez <18106359+DanielArturoAlejoAlvarez@users.noreply.github.com>
2025-02-19 20:08:53 -0500

    Update hello.dart

--- hello.dart
+++ hello.dart
@@ -1,14 +1,20 @@
-void main() {
-  print("Hi wave, I'm Daniel Arturo Alejo Alvarez.");
-  print("A passionate software developer from Perú.");
-  print("I’m currently working on MediasoftDev");
-  print("I’m currently learning Mobile Apps with Dart and Flutter.");
-  print("I’m looking to collaborate on Software projects that help companies improve their efficiency and productivity.");
-  print("I’m looking for help with You're open to new job opportunities.");
-  print("All of my projects are available at https://github.com/DanielArturoAlejoAlvarez.");
-  print("I regularly write articles on https://pe.linkedin.com/in/danielarturoalejoalvarez");
-  print("Ask me about Django, Spring, Laravel, Ruby on Rails, React, Angular, etc.");
-  print("How to reach me yourmediasoft@gmail.com");
-  print("Know about my experiences http://danapps.herokuapp.com/");
-  print("Fun fact I am passionate about technology and nature.");
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold (
+        appBar: AppBar(
+          title: Text('Welcome Flutter')
+        ),
+        body: Center(
+          child: Text('Innovation distinguishes between a leader and a follower.'),
+        ),
+      ),
+    );
+  }
 }
